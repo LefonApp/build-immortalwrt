40ff78613acbcf0d1777503c1ede69b648b01d10
LefonApp <46104566+LefonApp@users.noreply.github.com>
2021-08-16 17:10:16 +0800

    Update before_build.sh

--- before_build.sh
+++ before_build.sh
@@ -4,3 +4,6 @@ cd /home/runner/immortalwrt
 # Enter your commands here, e.g.
 # echo "Start build!"
 make defconfig
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
